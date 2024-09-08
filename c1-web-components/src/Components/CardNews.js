afe14cf2ba01d864c3b838d4457715cb325b094f
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-09-08 20:00:37 -0300

    Criando o TítuloDinâmico

--- c1-web-components/src/Components/CardNews.js
+++ c1-web-components/src/Components/CardNews.js
@@ -3,7 +3,7 @@ class CardNews extends HTMLElement{
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>HeLlo World</h1>";
+        shadow.innerHTML = "<h1>Hello World</h1>";
     }
 }
 
