16c38fcd5000fa1c3b8788744bab4b18c57d5eee
unknown <alexandrelorena>
2024-09-10 17:16:43 -0300

    Atualização CardNews

--- c1-web-components/src/Components/CardNews.js
+++ c1-web-components/src/Components/CardNews.js
@@ -16,8 +16,14 @@ class CardNews extends HTMLElement{
         cardLeft.setAttribute("class", "card_left");
        
         const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
         const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title"); 
+        linkTitle.href = this.getAttribute("link-url");
+
         const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
 
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
@@ -27,6 +33,8 @@ class CardNews extends HTMLElement{
         cardRight.setAttribute("class", "card_right");
 
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/foto-default.jpg";
+        newsImage.alt = "Foto da notícia";
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
