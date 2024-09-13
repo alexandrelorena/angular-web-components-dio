c552b97fa89b9b59b71768d850c74bb6aef86643
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-09-13 09:36:25 -0300

    Type Script - Tipos de variáveis

---
+++ typescript/src/tiposVariaveis.ts
@@ -0,0 +1,33 @@
+// ############### Tipos Primitivos ############### 
+
+//bollean
+let ligado: boolean = false; // só aceita valor do tipo referido
+
+let ligou= 1; // aceita qualquer valor pois não está tipado
+
+//string
+let nome: string = "Alexandre";
+
+let pessoa= 1; 
+
+//number
+let idade: number = 30;
+let altura: number = 1.70;
+
+// ############### Tipos Especiais ############### 
+
+//null
+let nulo: null = null; 
+
+//undefined
+let indefinido: undefined = undefined;
+
+// ############### Tipos abrangentes ############### 
+
+//void
+let retorno: void = undefined; //não retorna nada
+let retorna: void;
+function executaQuery(): void{};
+
+//any
+let retornoView: any = false; //pode retornar qualquer coisa
