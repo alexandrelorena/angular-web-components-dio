4051c4602ebb722f996b999afd80c685e9ef42a9
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-09-28 16:07:38 -0300

    Atualização Blog- componentes e pages

--- angular/projetos/angular-blog/src/app/app.module.ts
+++ angular/projetos/angular-blog/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MenuBarComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
