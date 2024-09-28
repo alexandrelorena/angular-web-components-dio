751146ee00a5e928045fce871ed03ff01789a24f
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-09-28 19:10:50 -0300

    Atualização 'components dynamics propertys

--- angular/projetos/angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular/projetos/angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ObservableInput } from 'rxjs';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
