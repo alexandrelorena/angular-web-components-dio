8a899d30522ccc564acdf71f4bb2c1bd46b95a28
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-10-03 09:41:33 -0300

    Atualização: Services

--- angular/services/README.md
+++ angular/services/README.md
@@ -1,6 +1,6 @@
 # Services
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.6.
 
 ## Development server
 
--- angular/services/src/app/app.component.ts
+++ angular/services/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Aula de Services</h1>
-  <app-card></app-card>
-  <router-outlet></router-outlet>
+    <h1>Aula de Services</h1>
+    <app-card></app-card>
+    <router-outlet></router-outlet>
   `,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'services';
