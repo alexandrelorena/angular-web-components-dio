bc471114cfb762c5a0e6c203802b21a87246ba33
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-10-06 20:01:32 -0300

    Atualização Services

--- angular/servicos/src/app/app.component.ts
+++ angular/servicos/src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Aula de Services</h1>
-    <app-card></app-card>
-    <router-outlet></router-outlet>
+    <div class="card-div">
+      <h1>Aula de Services</h1>
+      <app-card></app-card>
+      <router-outlet></router-outlet>
+    </div>
   `,
   styleUrl: './app.component.css',
 })
